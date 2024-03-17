f6db44f940d0c669ce12cd5c5672008e0ac740e3
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-17 22:42:17 +0900

    [#1] test: describe cases

---
+++ jehwan/week-1/src/point/point.service.spec.ts
@@ -0,0 +1,20 @@
+describe('PointService', () => {
+  describe('PointService.charge()', () => {
+    it.todo('Succeed to charge a point')
+    it.todo('Succeed to charge points')
+  })
+
+  describe('PointService.use()', () => {
+    it.todo('Succeed to use a point')
+    it.todo('Succeed to use points')
+    it.todo('Fail to use a point if the balance is insufficient')
+  })
+
+  describe('PointService.readPoint()', () => {
+    it.todo("Succeed to read a user's point")
+  })
+
+  describe('PointService.readHistories()', () => {
+    it.todo("Succeed to read user's histories")
+  })
+})
