016357bb13a52781cb9f29db416c10bf298c8af9
SGKwon95 <ssunder7@naver.com>
2024-03-19 21:11:47 +0900

    feat: generate UserPointService

--- soongyu/week-1/src/app.module.ts
+++ soongyu/week-1/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PointModule } from './point/point.module'
+import { UserPointService } from './userpoint/userpoint.service'
 
 @Module({
   imports: [PointModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserPointService],
 })
 export class AppModule {}
