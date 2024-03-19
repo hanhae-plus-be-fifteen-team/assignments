665fd30dac1bb785d3f592adb3614bd4f7f961c4
JHKANG9140 <upcoder0117@gmail.com>
2024-03-19 20:53:32 +0900

    test : 아이디별 포인트 조회

--- jeonghyo/week-1/src/point/point.module.ts
+++ jeonghyo/week-1/src/point/point.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { PointController } from './point.controller'
 import { DatabaseModule } from 'src/database/database.module'
-import { PointService } from './point.service/point.service';
+import { PointService } from './point.service';
 
 @Module({
   imports: [DatabaseModule],
