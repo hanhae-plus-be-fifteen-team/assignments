c705bf89ed728fae506d27b9ca9e71a559e2d1a6
shpark <5000roto@naver.com>
2024-03-19 20:26:41 +0900

    [#1] feat : history에 update

--- sohee/week-1/src/point/point.service.ts
+++ sohee/week-1/src/point/point.service.ts
@@ -20,7 +20,17 @@ export class PointService {
 
   async charge(id: number, amount: number): Promise<UserPoint> {
     const prevAmount = (await this.getOne(id)).point
-    return this.userDb.insertOrUpdate(id, prevAmount + amount)
+    const updatedUserPoint = await this.userDb.insertOrUpdate(
+      id,
+      prevAmount + amount,
+    )
+    await this.historyDb.insert(
+      id,
+      amount,
+      TransactionType.CHARGE,
+      updatedUserPoint.updateMillis,
+    )
+    return updatedUserPoint
   }
 
   async use(id: number, amount: number): Promise<UserPoint> {
@@ -29,7 +39,17 @@ export class PointService {
       console.log('포인트가 부족하여 사용할 수 없습니다.')
       return await this.getOne(id)
     } else {
-      return this.userDb.insertOrUpdate(id, prevAmount - amount)
+      const updatedUserPoint = await this.userDb.insertOrUpdate(
+        id,
+        prevAmount - amount,
+      )
+      await this.historyDb.insert(
+        id,
+        amount,
+        TransactionType.USE,
+        updatedUserPoint.updateMillis,
+      )
+      return updatedUserPoint
     }
   }
 }
