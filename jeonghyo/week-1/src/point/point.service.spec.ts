38fb2ed2e2a50e3cfb0161e5c617b628c5a3d324
JHKANG9140 <upcoder0117@gmail.com>
2024-03-19 20:53:32 +0900

    test : 아이디별 포인트 조회

--- jeonghyo/week-1/src/point/point.service.spec.ts
+++ jeonghyo/week-1/src/point/point.service.spec.ts
@@ -2,17 +2,30 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PointService } from './point.service';
 
 describe('PointService', () => {
-  let provider: PointService;
+  let service: PointService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PointService],
     }).compile();
 
-    provider = module.get<PointService>(PointService);
+    service = module.get<PointService>(PointService);
   });
 
   it('should be defined', () => {
-    expect(provider).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  describe('PointService.point()', () => {
+    beforeEach(async () => {
+      const userId = 1;
+      await service.charge(userId, 1000);
+    });
+    // 포인트 입력 테스트
+    it('Point Check', async  () => {
+      const userPoint = await service.point(1);
+      expect(userPoint.id).toEqual(1);
+      expect(userPoint.point).toEqual(1000);
+    });
   });
 });
