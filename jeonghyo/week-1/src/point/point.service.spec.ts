a6cff12de26c7d720a13a75c70daa58f6845ae72
JHKANG9140 <upcoder0117@gmail.com>
2024-03-19 20:54:32 +0900

    test : 충전/사용 내역 조회

--- jeonghyo/week-1/src/point/point.service.spec.ts
+++ jeonghyo/week-1/src/point/point.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PointService } from './point.service';
+import { TransactionType } from './point.model';
 
 describe('PointService', () => {
   let service: PointService;
@@ -26,6 +27,25 @@ describe('PointService', () => {
       const userPoint = await service.point(1);
       expect(userPoint.id).toEqual(1);
       expect(userPoint.point).toEqual(1000);
+    });
+  });
+
+  describe('PointService.history()', () => {
+    beforeEach(async () => {
+      await service.charge(1, 10000);
+      await service.charge(2, 10000);
+      await service.use(1, 1000);
+      await service.use(2, 1000);
+    })
+    it('Point History Check', async () => {    
+      // 내역 검색 테스트
+      const userHistory_1 = await service.history(1);
+      expect(userHistory_1).toBeInstanceOf(Array);
+      expect(userHistory_1.length).toEqual(2);
+      expect(userHistory_1[0].userId).toEqual(1);
+      expect(userHistory_1[0].type).toEqual(TransactionType.CHARGE);
+      expect(userHistory_1[1].userId).toEqual(1);
+      expect(userHistory_1[1].type).toEqual(TransactionType.USE);
     });
   });
 });
