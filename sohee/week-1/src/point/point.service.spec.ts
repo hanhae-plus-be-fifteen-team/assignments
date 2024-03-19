39d7c70bf4e390f0ea5c5bbda764e38f9677b32a
shpark <5000roto@naver.com>
2024-03-19 20:26:09 +0900

    [#1] test : get by id

--- sohee/week-1/src/point/point.service.spec.ts
+++ sohee/week-1/src/point/point.service.spec.ts
@@ -19,4 +19,23 @@ describe('PointService', () => {
     expect(service).toBeDefined()
   })
 
+  describe('get', () => {
+    const userId = 1
+    const amount = 0
+
+    it('succeed to get', async () => {
+      await service.charge(userId, amount)
+      const result = await service.getOne(userId).then(_ => _.point)
+      expect(amount).toEqual(result)
+    })
+
+    it('fail to get', async () => {
+      await service.charge(userId, amount)
+      try {
+        await service.getOne(2).then(_ => _.point)
+      } catch (e) {
+        expect(e).toBe('올바르지 않은 ID 값 입니다.')
+      }
+    })
+  })
 })
