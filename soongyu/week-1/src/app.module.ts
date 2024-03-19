a95d0ea7aaef8e26c7bf16a513504e4d4e594df9
SGKwon95 <ssunder7@naver.com>
2024-03-20 00:05:44 +0900

    chore: remove unused code

--- soongyu/week-1/src/app.module.ts
+++ soongyu/week-1/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PointModule } from './point/point.module'
-import { UserPointService } from './point/point.service'
 
 @Module({
   imports: [PointModule],
-  controllers: [AppController],
-  providers: [AppService, UserPointService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
