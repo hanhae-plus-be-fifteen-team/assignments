136608ce4655fde82ffe1fc4cdb1c7702d8a46f2
SGKwon95 <ssunder7@naver.com>
2024-03-19 21:24:47 +0900

    feat: inject UserPointService

--- soongyu/week-1/src/userpoint/userpoint.service.ts
+++ soongyu/week-1/src/userpoint/userpoint.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { PointHistoryTable } from 'src/database/pointhistory.table'
 import { UserPointTable } from 'src/database/userpoint.table'
 import { UserPoint, PointHistory } from 'src/point/point.model'
+import { PointBody as PointDto } from '../point/point.dto'
 
 @Injectable()
 export class UserPointService {
@@ -9,4 +10,17 @@ export class UserPointService {
     private readonly userDb: UserPointTable,
     private readonly historyDb: PointHistoryTable,
   ) {}
+
+  async chargePoint(id: number, pointDto: PointDto): Promise<UserPoint> {
+    return null
+  }
+  async usePoint(id: number, pointDto: PointDto): Promise<UserPoint> {
+    return null
+  }
+  async getUserPoint(id: number): Promise<UserPoint> {
+    return await this.userDb.selectById(id)
+  }
+  async getUserPointHistory(id: number): Promise<PointHistory[]> {
+    return await this.historyDb.selectAllByUserId(id)
+  }
 }
