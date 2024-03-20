9ec72f8e278f3a5ee9f53905f60bc4d5a2ad6c6c
shpark <5000roto@naver.com>
2024-03-20 12:48:19 +0900

    [#1] chore : cleanup

--- sohee/week-1/src/app.module.ts
+++ sohee/week-1/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PointModule } from './point/point.module'
 
 @Module({
   imports: [PointModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
