5e95d0774ee1a587c1d181f9cc910aeb25d867b4
JHKANG9140 <upcoder0117@gmail.com>
2024-03-20 22:16:50 +0900

    [#1] refactor: charge, use 진행 중 에러 발생 시 원복

--- jeonghyo/week-1/src/point/point.service.ts
+++ jeonghyo/week-1/src/point/point.service.ts
@@ -23,25 +23,43 @@ export class PointService {
         userId: number,
         amount: number,
     ): Promise<UserPoint> {
-        if (amount < 0) throw new Error('amount should larger then 0');
-        const beforeUpdate = await this.userDb.selectById(userId);
-        const calcPoint = beforeUpdate.point + amount;
-        const afterUpdate = await this.userDb.insertOrUpdate(userId, calcPoint);
-        await this.historyDb.insert(userId, amount, TransactionType.CHARGE, afterUpdate.updateMillis);
-        return afterUpdate;
+        let beforeUpdate: UserPoint;
+        try{
+            if (amount < 0) throw new Error('amount should larger then 0');
+            beforeUpdate = await this.userDb.selectById(userId);
+            const calcPoint = beforeUpdate.point + amount;
+            const afterUpdate = await this.userDb.insertOrUpdate(userId, calcPoint);
+            await this.historyDb.insert(userId, amount, TransactionType.CHARGE, afterUpdate.updateMillis);
+            return afterUpdate;
+        } catch(e) {
+            if (beforeUpdate) {
+                // 에러가 발생하면 업데이트 이전의 데이터를 복원
+                await this.userDb.insertOrUpdate(userId, beforeUpdate.point);
+            }
+            throw e;
+        }
     }
 
     async use(
         userId: number,
         amount: number,
     ): Promise<UserPoint> {
-        if (amount < 0) throw new Error('amount should larger then 0')
-        const beforeUpdate = await this.userDb.selectById(userId);
-        const calcPoint = beforeUpdate.point - amount
-        if (calcPoint < 0) throw new Error('Balance Insufficient')
-        const afterUpdate = await this.userDb.insertOrUpdate(userId, calcPoint);
-        await this.historyDb.insert(userId, amount, TransactionType.USE, afterUpdate.updateMillis);
-        return afterUpdate;
+        let beforeUpdate: UserPoint;
+        try {
+            if (amount < 0) throw new Error('amount should larger then 0')
+            beforeUpdate = await this.userDb.selectById(userId);
+            const calcPoint = beforeUpdate.point - amount
+            if (calcPoint < 0) throw new Error('Balance Insufficient')
+            const afterUpdate = await this.userDb.insertOrUpdate(userId, calcPoint);
+            await this.historyDb.insert(userId, amount, TransactionType.USE, afterUpdate.updateMillis);
+            return afterUpdate;
+        } catch(e) {
+            if (beforeUpdate) {
+                // 에러가 발생하면 업데이트 이전의 데이터를 복원
+                await this.userDb.insertOrUpdate(userId, beforeUpdate.point);
+            }
+            throw e;
+        }
     }
     
 }
--- jeonghyo/week-1/test/app.e2e-spec.ts
+++ jeonghyo/week-1/test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
+import { TransactionType } from '../src/point/point.model';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
@@ -15,10 +16,57 @@ describe('AppController (e2e)', () => {
     await app.init()
   })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!')
-  })
+  describe('PointService Test', () => {
+    beforeEach(async () => {
+      await request(app.getHttpServer())
+        .patch('/point/1/charge').send({ amount: 1000 });
+    });
+    // 음수 입력 상황 예외 처리
+    it('Minus Charge Test', async () => {
+      await request(app.getHttpServer())
+      .patch('/point/1/charge')
+      .send({
+        amount: -1000,
+      })
+      .expect(500);
+    });
+    // 음수 입력 상황 예외 처리
+    it('Minus Use Test', async () => {
+      await request(app.getHttpServer())
+      .patch('/point/1/use')
+      .send({
+        amount: -1000,
+      })
+      .expect(500);
+    });
+    // 보유 포인트보다 많이 사용할 때 예외 처리
+    it('Use More Then Charged Point Test', async () => {
+      await request(app.getHttpServer())
+      .patch('/point/1/use')
+      .send({
+        amount: 2000,
+      })
+      .expect(500);
+    });
+  });
+
+  describe('Sequential Execution Test', () => {
+    beforeEach(async () => {
+      await request(app.getHttpServer())
+        .patch('/point/1/charge').send({ amount: 1000 });
+      await request(app.getHttpServer())
+        .patch('/point/1/use').send({ amount: 500 });
+      await request(app.getHttpServer())
+        .patch('/point/1/charge').send({ amount: 1000 });
+    });
+    // 입력 순서대로 실행되는지 확인
+    it('History Order', async () => {
+      const res = await request(app.getHttpServer())
+        .get('/point/1/histories')
+        .expect(200);
+      expect(res.body[0].type).toBe(TransactionType.CHARGE);
+      expect(res.body[1].type).toBe(TransactionType.USE);
+      expect(res.body[2].type).toBe(TransactionType.CHARGE);
+    });
+  });
 })
