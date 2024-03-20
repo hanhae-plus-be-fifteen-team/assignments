377fd90457b0c62da910997226552c4eb1d5bdbc
shpark <5000roto@naver.com>
2024-03-20 10:25:04 +0900

    [#1] feat : amount 음수 case 추가

--- sohee/week-1/src/point/point.service.ts
+++ sohee/week-1/src/point/point.service.ts
@@ -40,22 +40,19 @@ export class PointService {
   }
 
   async use(id: number, amount: number): Promise<UserPoint> {
-    const prevAmount = (await this.getOne(id)).point
-    if (prevAmount - amount < 0) {
-      console.log('포인트가 부족하여 사용할 수 없습니다.')
-      return await this.getOne(id)
-    } else {
-      const updatedUserPoint = await this.userDb.insertOrUpdate(
-        id,
-        prevAmount - amount,
-      )
-      await this.historyDb.insert(
-        id,
-        amount,
-        TransactionType.USE,
-        updatedUserPoint.updateMillis,
-      )
-      return updatedUserPoint
-    }
+    const prev = await this.getOne(id)
+    if (amount < 0) throw new BadRequestException()
+    if (prev.point - amount < 0) return await this.getOne(id)
+    const updatedUserPoint = await this.userDb.insertOrUpdate(
+      id,
+      prev.point - amount,
+    )
+    await this.historyDb.insert(
+      id,
+      amount,
+      TransactionType.USE,
+      updatedUserPoint.updateMillis,
+    )
+    return updatedUserPoint
   }
 }
