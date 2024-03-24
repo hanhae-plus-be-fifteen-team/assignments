bd226310be33ec4fc47fb9c3552ad23f8629996b
SGKwon95 <ssunder7@naver.com>
2024-03-25 03:16:38 +0900

    [#2] chore: config typeorm

--- soongyu/week-2/src/app.module.ts
+++ soongyu/week-2/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common'
+import { CourseModule } from './course/course.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'hanghaeplus',
+      entities: [__dirname + '/**/*.entry{.ts,.js}'],
+      synchronize: true,
+    }),
+    CourseModule,
+  ],
   controllers: [],
   providers: [],
 })
