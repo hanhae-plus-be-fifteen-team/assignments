5f299e6333c59b90c1a883386f5dc27c406c6990
shpark <5000roto@naver.com>
2024-03-25 16:39:18 +0900

    [#2] feat : enroll service 구현

--- sohee/week-2/src/enrollment/enrollment.service.ts
+++ sohee/week-2/src/enrollment/enrollment.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { EnrollResult } from './enrollment.model'
+import { EnrollmentRepository } from './repository/enrollment.repository'
 
 @Injectable()
-export class EnrollmentService {}
+export class EnrollmentService {
+  constructor(private readonly enrollRepository: EnrollmentRepository) {}
+
+  async enroll(studentId: string, classId: number): Promise<EnrollResult> {
+    return await this.enrollRepository.enroll(studentId, classId)
+  }
+
+  async getClasses(studentId: string): Promise<number[]> {
+    return await this.enrollRepository.getClasses(studentId)
+  }
+}
