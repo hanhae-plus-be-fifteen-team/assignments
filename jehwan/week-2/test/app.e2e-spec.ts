54e85434e612dc1a9546c8ffba7be2bb247064e1
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-25 21:09:57 +0900

    [#2] test: postgres health check

--- jehwan/week-2/test/app.e2e-spec.ts
+++ jehwan/week-2/test/app.e2e-spec.ts
@@ -2,9 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from '../src/app.module'
+import { GenericContainer, StartedTestContainer } from 'testcontainers'
+import { Pool } from 'pg'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
+  let container: StartedTestContainer | null = null
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +18,40 @@ describe('AppController (e2e)', () => {
     await app.init()
   })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!')
+  beforeEach(async () => {
+    container = await new GenericContainer('postgres:latest')
+      .withExposedPorts(5432)
+      .withEnvironment({
+        POSTGRES_USER: 'user',
+        POSTGRES_PASSWORD: 'password',
+        POSTGRES_DB: 'test',
+      })
+      .withBindMounts([
+        {
+          source: process.cwd() + '/scripts/init-db.sql',
+          target: '/docker-entrypoint-initdb.d/init.sql',
+        },
+      ])
+      .start()
+  }, 60000)
+
+  afterEach(async () => {
+    if (container !== null) {
+      await container.stop()
+    }
+
+    await app.close()
+  })
+
+  it('database health check', async () => {
+    expect(container).toBeDefined()
+
+    const connectionStr = `postgresql://user:password@localhost:5432/test`
+    const db = new Pool({ connectionString: connectionStr })
+
+    const result = await db.query('SELECT NOW()')
+    expect(result.rows).toBeDefined()
+
+    await db.end()
   })
 })
