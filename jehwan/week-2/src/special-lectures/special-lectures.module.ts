2ff1df4364c8534e951e51f832376348ed563030
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-25 16:58:59 +0900

    [#2] feat(controller): implement SpecialLecturesController

--- jehwan/week-2/src/special-lectures/special-lectures.module.ts
+++ jehwan/week-2/src/special-lectures/special-lectures.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AdaptationService } from '../adaptation/adaptation.service'
 import { SpecialLecturesServiceAdapter } from './special-lectures.service.adapter'
+import { SpecialLecturesController } from './special-lectures.controller'
 
 @Module({
+  controllers: [SpecialLecturesController],
   providers: [AdaptationService, SpecialLecturesServiceAdapter],
 })
 export class SpecialLecturesModule {}
