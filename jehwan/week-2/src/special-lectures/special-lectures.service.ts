71bbf1697a8635ae1b487f335231b178d5dbae2d
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-25 14:48:14 +0900

    [#2] feat(service): init SpecialLecturesService

---
+++ jehwan/week-2/src/special-lectures/special-lectures.service.ts
@@ -0,0 +1,21 @@
+import { SpecialLectureApplicationResult } from './special-lectures.model'
+
+export class SpecialLecturesService {
+  /**
+   *
+   * @param applicantId - applicant's id for the lecture
+   * @returns the result of the application
+   */
+  apply(applicantId: number): Promise<SpecialLectureApplicationResult> {
+    return
+  }
+
+  /**
+   *
+   * @param applicantId - applicant's id for the lecture
+   * @returns the result of the application
+   */
+  read(applicantId: number): Promise<SpecialLectureApplicationResult> {
+    return
+  }
+}
