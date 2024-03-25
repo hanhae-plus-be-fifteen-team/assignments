db4a0eb57f9e6471c5f179a89cb22601c855ee06
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-25 19:24:39 +0900

    [#2] feat(service): inject repository impl into the service

--- jehwan/week-2/src/special-lectures/special-lectures.service.adapter.ts
+++ jehwan/week-2/src/special-lectures/special-lectures.service.adapter.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { SpecialLecturesService } from './special-lectures.service'
 import { AdaptationService } from '../adaptation/adaptation.service'
+import { SpecialLecturesRepositoryImpl } from './special-lectures.repository.impl'
 
 @Injectable()
 export class SpecialLecturesServiceAdapter {
   readonly service: SpecialLecturesService
 
   constructor(private adaptationService: AdaptationService) {
-    this.service = this.adaptationService.adapt(SpecialLecturesService)
+    this.service = this.adaptationService.adapt(SpecialLecturesService, [
+      SpecialLecturesRepositoryImpl,
+    ])
   }
 }
