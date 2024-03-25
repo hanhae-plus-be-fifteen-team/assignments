5072be6eb40523413e704774c12d4276c7e7c352
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-25 16:52:29 +0900

    [#2] feat(service): SpecialLecturesServiceAdapter

--- jehwan/week-2/src/special-lectures/special-lectures.module.ts
+++ jehwan/week-2/src/special-lectures/special-lectures.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common'
+import { AdaptationService } from '../adaptation/adaptation.service'
+import { SpecialLecturesServiceAdapter } from './special-lectures.service.adapter'
 
-@Module({})
+@Module({
+  providers: [AdaptationService, SpecialLecturesServiceAdapter],
+})
 export class SpecialLecturesModule {}
