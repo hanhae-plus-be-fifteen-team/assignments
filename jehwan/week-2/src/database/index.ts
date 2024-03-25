a6b22cf1daa59daa0793c4f0e1b177ccf4658982
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-25 19:49:12 +0900

    [#2] fix(database): wrong configuration

--- jehwan/week-2/src/database/index.ts
+++ jehwan/week-2/src/database/index.ts
@@ -6,6 +6,6 @@ export function createConnection() {
     port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    connectionString: process.env.DB_CONNECTION_STRING,
+    database: process.env.DB_NAME,
   })
 }
