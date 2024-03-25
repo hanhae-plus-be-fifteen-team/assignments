a471a6505eae0712272108274db4a44b4925ae5d
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-25 14:53:34 +0900

    [#2] feat(repository): define SpecialLecturesRepository interface

---
+++ jehwan/week-2/src/special-lectures/special-lectures.repository.ts
@@ -0,0 +1,21 @@
+import { SpecialLectureApplicationResult } from './special-lectures.model'
+
+export interface SpecialLecturesRepository {
+  /**
+   *
+   * @param userId  applicant's id for the lecture
+   * @description
+   * Internally, use the concept of [JS Set](https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Set)
+   * to maintain the order of applications while ensuring there are no duplicates.
+   */
+  pushApplicantIntoLecture(userId: number): Promise<void>
+
+  /**
+   *
+   * @param userId  applicant's id for the lecture
+   * @returns the result of the application
+   */
+  readResultOfApplicant(
+    userId: number,
+  ): Promise<SpecialLectureApplicationResult>
+}
