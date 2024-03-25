e8283fe1ec936064f9ef86ba197861bf233b1e27
shpark <5000roto@naver.com>
2024-03-25 16:39:49 +0900

    [#2] init : provider 추가 및 pkg 추가

--- sohee/week-2/src/enrollment/enrollment.module.ts
+++ sohee/week-2/src/enrollment/enrollment.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
 import { EnrollmentController } from './enrollment.controller'
 import { EnrollmentService } from './enrollment.service'
+import { EnrollmentRepository } from './repository/enrollment.repository'
+import { EnrollmentTable } from 'src/database/enrollment.table'
 
 @Module({
   controllers: [EnrollmentController],
-  providers: [EnrollmentService],
+  providers: [
+    EnrollmentService,
+    EnrollmentRepository,
+    // { provide: 'IEnrollmentRepository', useClass: EnrollmentRepository },
+    EnrollmentTable,
+  ],
 })
 export class EnrollmentModule {}
