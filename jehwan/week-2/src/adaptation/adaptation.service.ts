e394d8a8925510860050ea3e94063a41f8f50e2d
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-25 19:22:35 +0900

    [#2] feat(common): implement custom providers

--- jehwan/week-2/src/adaptation/adaptation.service.ts
+++ jehwan/week-2/src/adaptation/adaptation.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AdaptationService {
-  public adapt<T>(service: new (...args: any[]) => T): T {
-    return new service()
+  public adapt<S, P>(
+    service: new (...args: unknown[]) => S,
+    providers?: (new (...args: unknown[]) => P)[],
+  ): S {
+    return new service(...(providers?.map(p => new p()) ?? []))
   }
 }
