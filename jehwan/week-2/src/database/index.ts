e231dcd1d5be508db1d33aaa38b0a5e22bbceb5e
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-26 21:49:21 +0900

    [#2] refactor: read test

--- jehwan/week-2/src/database/index.ts
+++ jehwan/week-2/src/database/index.ts
@@ -2,6 +2,13 @@ import * as pg_promise from 'pg-promise'
 
 const pgp = pg_promise({})
 
+/**
+ * @todo 원인 분석이 필요함.
+ *  WARNING: Creating a duplicate database object for the same connection.
+ *         at createDb (/Users/yoojehwan/WebstormProjects/hhplus-tdd-nest/jehwan/week-2/src/database/index.ts:6:10)
+ *         at Object.<anonymous> (/Users/yoojehwan/WebstormProjects/hhplus-tdd-nest/jehwan/week-2/test/app.e2e-spec.ts:92:26)
+ *         at processTicksAndRejections (node:internal/process/task_queues:95:5)
+ */
 export function createDb() {
   return pgp({
     host: process.env.DB_HOST ?? 'localhost',
