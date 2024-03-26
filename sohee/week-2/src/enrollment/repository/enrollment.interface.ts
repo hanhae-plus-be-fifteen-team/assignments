1a703ebc81427a77d36121c8c0e55539ef1e636e
shpark <5000roto@naver.com>
2024-03-26 17:08:06 +0900

    [#2] init : cleanup

--- sohee/week-2/src/enrollment/repository/enrollment.interface.ts
+++ sohee/week-2/src/enrollment/repository/enrollment.interface.ts
@@ -1,4 +1,4 @@
-import { EnrollResult } from '../enrollment.model'
+import { EnrollResult } from '../models/enrollment.result'
 
 export interface IEnrollmentRepository {
   enroll(studentId: string, classId: number): Promise<EnrollResult>
