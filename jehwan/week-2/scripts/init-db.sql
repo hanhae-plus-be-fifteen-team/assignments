bced4f938ae5a53dd52d8fe280f1d00d01a9b0a7
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-27 17:44:22 +0900

    [#2] refactor: init-db.sql with v4

--- jehwan/week-2/scripts/init-db.sql
+++ jehwan/week-2/scripts/init-db.sql
@@ -1,6 +1,18 @@
 create table if not exists "special_lectures" (
   id serial primary key,
-  user_id int unique,
-  applied boolean not null,
-  created_at timestamp default now()
+  title varchar(255) not null,
+  opening_date timestamp not null
+);
+
+create table if not exists "special_lectures_count" (
+  lecture_id int primary key references special_lectures(id),
+  maximum int not null,
+  count int not null
+);
+
+create table if not exists "applications" (
+  lecture_id int not null references special_lectures(id),
+  user_id int not null,
+  created_at timestamp not null,
+  primary key (lecture_id, user_id)
 );
