b85d19c4a313c74a4268e8c79df3c9d9c672c0e1
shpark <5000roto@naver.com>
2024-03-27 16:39:52 +0900

    [#2] refactor : interface로 repository 접근

--- sohee/week-2/src/enrollment/enrollment.service.spec.ts
+++ sohee/week-2/src/enrollment/enrollment.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { EnrollmentService } from './enrollment.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { EnrollmentController } from './enrollment.controller'
+import { Enrollment } from './models/enrollment.entities'
+import { EnrollmentRepository } from './repository/enrollment.repository'
 
 describe('EnrollmentService', () => {
   let service: EnrollmentService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EnrollmentService],
+      imports: [TypeOrmModule.forFeature([Enrollment, EnrollmentRepository])],
+      controllers: [EnrollmentController],
+      providers: [EnrollmentService, EnrollmentRepository],
     }).compile()
 
     service = module.get<EnrollmentService>(EnrollmentService)
--- sohee/week-2/src/enrollment/repository/enrollment.interface.ts
+++ sohee/week-2/src/enrollment/repository/enrollment.interface.ts
@@ -1,5 +1,6 @@
 import { EnrollResult } from '../models/enrollment.result'
 
+export const IENROLLMENT_REPOSITORY = 'Enrollment Repository'
 export interface IEnrollmentRepository {
   enroll(studentId: string, classId: number): Promise<EnrollResult>
   getClasses(studentId: string): Promise<number[]>
