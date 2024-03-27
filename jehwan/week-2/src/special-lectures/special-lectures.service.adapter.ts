1017dfbb0b9e69830b6d10e3a31a78699d671dcd
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-03-27 11:25:40 +0900

    [#2] docs: SpecialLecturesServiceAdapter

--- jehwan/week-2/src/special-lectures/special-lectures.service.adapter.ts
+++ jehwan/week-2/src/special-lectures/special-lectures.service.adapter.ts
@@ -5,9 +5,18 @@ import { SpecialLecturesRepositoryImpl } from './special-lectures.repository.imp
 
 @Injectable()
 export class SpecialLecturesServiceAdapter {
+  /**
+   * @todo
+   * Don't use SpecialLecturesService directly,
+   * declare the interface of SpecialLecturesService
+   */
   readonly service: SpecialLecturesService
 
   constructor(private adaptationService: AdaptationService) {
+    /**
+     * @description
+     * Integrate SpecialLecturesService written purely in TypeScript into the Nest application.
+     */
     this.service = this.adaptationService.adapt(SpecialLecturesService, [
       SpecialLecturesRepositoryImpl,
     ])
